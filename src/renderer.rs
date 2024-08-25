72d637a809a8338989e84f5a1d5a0f676595f9c0
blookewoost <blakewest1005@gmail.com>
2024-08-25 19:25:27 -0400

    basic player movement with attached camera

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, render::{mesh::VertexAttributeValues, render_asset::RenderAssetUsages}, transform::commands};
-use las::{point, Reader};
-use bevy::render::mesh::{Indices, Mesh, PrimitiveTopology};
+use bevy::{ecs::query, prelude::*, render::render_asset::RenderAssetUsages};
+use las::Reader;
+use bevy::render::mesh::{Mesh, PrimitiveTopology};
 
 use crate::Sector;
 
@@ -8,6 +8,9 @@ use crate::Sector;
 struct PointCloudMesh {
     mesh: Mesh
 }
+
+#[derive(Component)]
+struct Player;
 
 pub fn bevy_test(sector: Sector) {
     let mut commands: Commands;
@@ -18,6 +21,7 @@ pub fn bevy_test(sector: Sector) {
     .add_plugins(DefaultPlugins)
     .insert_resource(PointCloudMesh{ mesh: pc_mesh })
     .add_systems(Startup, setup)
+    .add_systems(Update, player_movement)
     .run();
 }
 
@@ -28,15 +32,45 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, newmesh: Res<
         ..Default::default()
     });
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(950000.0, 820000.0, 1000.0).looking_at(Vec3::ZERO, Vec3::Y),
-        
-        ..Default::default()
+    commands.spawn(TransformBundle::from_transform(Transform::from_xyz(950000.0, 820000.0, 500.0)))
+    .insert(Player)
+    .with_children(|player| {
+        player.spawn( Camera3dBundle{
+            transform: Transform::from_xyz(0.0, 0.0, 0.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        });
     });
 }
 
-fn some_function() {
-    println!("Hello World!");
+fn player_movement(keys: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<&mut Transform, With<Player>>) {
+    for mut transform in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keys.pressed(KeyCode::KeyW) {
+            direction.y += 10.0;
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            direction.x += 10.0;
+        }
+        if keys.pressed(KeyCode::KeyS) {
+            direction.y -= 10.0;
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            direction.x -= 10.0;
+        }
+        if keys.pressed(KeyCode::ArrowUp) {
+            direction.z += 5.0;
+        }
+        if keys.pressed(KeyCode::ArrowDown) {
+            direction.z -= 5.0;
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+            transform.translation += direction * time.delta_seconds() * 200.0;
+        }
+    }
 }
 
 fn create_pc_mesh(sector: Sector) -> Mesh {
@@ -50,8 +84,3 @@ fn create_pc_mesh(sector: Sector) -> Mesh {
     .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, points);
     mesh
 }   
-
-fn render_sector(sector: Sector) {
-    let reader = Reader::from_path(sector.data_file).unwrap();
-
-}
